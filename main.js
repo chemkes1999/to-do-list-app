fc457ed1322fef0243df823d81f82b9b982ce96c
Carlos Hemkes <c.hemkes1999@gmail.com>
2023-07-28 11:16:26 -0600

    se agrega el to do list

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+// Obtenemos referencias a los elementos del DOM
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('new-task');
+const taskList = document.getElementById('task-list');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Función para agregar una nueva tarea a la lista
+function addTask(event) {
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
 
-setupCounter(document.querySelector('#counter'))
+    if (taskText !== '') {
+        const taskItem = document.createElement('li');
+        taskItem.innerText = taskText;
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
+    }
+}
+
+// Event listener para el formulario de agregar tarea
+taskForm.addEventListener('submit', addTask);
