66b72723d5e93e5dc42de1067fd79efe55c413ea
Carlos Hemkes <c.hemkes1999@gmail.com>
2023-07-28 11:27:10 -0600

    se agrega el metodo exportar a csv

--- main.js
+++ main.js
@@ -30,3 +30,22 @@ function addTask(event) {
 
 // Event listener para el formulario de agregar tarea
 taskForm.addEventListener('submit', addTask);
+
+// Función para exportar las tareas como una tabla en formato CSV
+function exportTasksToCSV() {
+    const tasks = Array.from(taskList.getElementsByTagName('li'));
+    const csvContent = "data:text/csv;charset=utf-8," + tasks.map(task => task.innerText).join('\n');
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', 'tasks.csv');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// Agregamos un evento para exportar las tareas cuando se haga clic en un botón
+const exportButton = document.createElement('button');
+exportButton.innerText = 'Exportar CSV';
+exportButton.addEventListener('click', exportTasksToCSV);
+document.getElementById('app').appendChild(exportButton);
