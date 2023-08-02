afb35845dca2fb539958bc8441b6fd1ade83391d
Carlos Hemkes <c.hemkes1999@gmail.com>
2023-08-01 22:04:05 -0600

    fix

--- main.js
+++ main.js
@@ -5,48 +5,46 @@ const taskList = document.getElementById("task-list");
 
 // Función para agregar una nueva tarea a la lista
 function addTask(event) {
-  event.preventDefault();
-  const taskText = taskInput.value.trim();
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
 
-  if (taskText !== "") {
-    const taskItem = document.createElement("li");
-    taskItem.innerText = taskText;
+    if (taskText !== "") {
+        const taskItem = document.createElement("li");
+        taskItem.innerText = taskText;
 
-    // Creamos un botón para eliminar la tarea
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "Eliminar";
-    deleteButton.addEventListener("click", () => {
-      taskList.removeChild(taskItem);
-    });
+        // Creamos un botón para eliminar la tarea
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Eliminar";
+        deleteButton.addEventListener("click", () => {
+            taskList.removeChild(taskItem);
+        });
 
-    // Agregamos el botón al elemento de la tarea
-    taskItem.appendChild(deleteButton);
+        // Agregamos el botón al elemento de la tarea
+        taskItem.appendChild(deleteButton);
 
-    // Agregamos la tarea a la lista
-    taskList.appendChild(taskItem);
-    taskInput.value = "";
-    mostrarAlertaSucces(taskText);
-  } else {
-    mostrarAlertaError(taskText);
-  }
+        // Agregamos la tarea a la lista
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+        mostrarAlertaSucces(taskText);
+    } else {
+        mostrarAlertaError(taskText);
+    }
 
-  function mostrarAlertaSucces(taskText) {
-    Swal.fire({
-      title: "Tarea agregada",
-      text: "Se ha agregado la tarea "+taskText,
-      icon: "success",
-      confirmButtonText: "Ok",
-    });
-  }
+    function mostrarAlertaSucces(taskText) {
+        Swal.fire({
+            title: "Tarea agregada",
+            text: "Se ha agregado la tarea " + taskText,
+            icon: "success",
+            confirmButtonText: "Ok",
+        });
+    }
 
-  function mostrarAlertaError() {
-    Swal.fire({
-      title: "¡Error!",
-      text: "No puede estar vacio el nombre de la tarea.",
-      icon: "error", // Cambiamos el icono a 'error'
-      confirmButtonText: "Ok",
-    });
-  }
+    function mostrarAlertaError() {
+        Swal.fire({
+            title: "¡Error!", text: "No puede estar vacio el nombre de la tarea.", icon: "error", // Cambiamos el icono a 'error'
+            confirmButtonText: "Ok",
+        });
+    }
 }
 
 // Event listener para el formulario de agregar tarea
@@ -54,17 +52,15 @@ taskForm.addEventListener("submit", addTask);
 
 // Función para exportar las tareas como una tabla en formato CSV
 function exportTasksToCSV() {
-  const tasks = Array.from(taskList.getElementsByTagName("li"));
-  const csvContent =
-    "data:text/csv;charset=utf-8," +
-    tasks.map((task) => task.innerText).join("\n");
-  const encodedUri = encodeURI(csvContent);
-  const link = document.createElement("a");
-  link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "tasks.csv");
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+    const tasks = Array.from(taskList.getElementsByTagName("li"));
+    const csvContent = "data:text/csv;charset=utf-8," + tasks.map((task) => task.innerText).join("\n");
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "tasks.csv");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
 
 // Crear el botón de exportar
